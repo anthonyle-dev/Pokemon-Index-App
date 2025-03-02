c7e2006e1d5fa40bf2d77cbdab3ead654639081c
anthonyle-dev <anthonyle.dev@gmail.com>
2025-03-01 20:27:49 -0500

    set up MongoDB database and established connection to app

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,26 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: join(__dirname, '..', '.env') });
 
-app.listen(5173, () => {
-        console.log("Server is running on port 5173!");
-}); 
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 3000;
+
+if(!MONGODB_URI) {
+      console.error("MongoDB URI not found in environment variables.");
+      process.exit(1);
+}
+
+mongoose
+      .connect(MONGODB_URI)
+      .then(() => {
+        console.log("Connected to MongoDB");
+       app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error("Database connection error:", err));
